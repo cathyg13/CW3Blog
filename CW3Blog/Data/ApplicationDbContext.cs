61a8647597c771a2982af18f40899a6979bd7c15
CATHRYN GROVES <927289@Swansea.ac.uk>
2018-12-29 17:24:50 +0000

    Created class User and UsersController

--- CW3Blog/Data/ApplicationDbContext.cs
+++ CW3Blog/Data/ApplicationDbContext.cs
@@ -22,5 +22,7 @@ namespace CW3Blog.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<CW3Blog.Models.User> User { get; set; }
     }
 }
---
+++ CW3Blog/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW3Blog.Models
+{
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+        public enum Role { Customer = 0, Admin = 1 }
+            public Role {get; set;}
+        public enum Gender { Male = 0, Female = 1, PreferNotToSay = 3 }
+            public Gender Gender { get; set; }
+        public enum Location { UnitedKingdom = 0 }
+            public Location Location { get; set; }
+        public string DoB { get; set; }
+        public string Avatar { get; set; }
+        //link to \images
+    }
+}
