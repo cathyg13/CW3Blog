049154feb880217431693b967504fc36c7a85e1d
CATHRYN GROVES <927289@Swansea.ac.uk>
2018-12-29 15:55:03 +0000

    Add project files.

---
+++ CW3Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CW3Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
