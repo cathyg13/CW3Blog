cbc7fb06cf05d8fe2a09966198aaf0b2c6ffaef8
CATHRYN GROVES <927289@Swansea.ac.uk>
2019-01-05 14:50:43 +0000

    Made analytics page more user friendly

--- CW3Blog/ViewModels/AnalyticsViewModel.cs
+++ CW3Blog/ViewModels/AnalyticsViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using CW3Blog.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CW3Blog.ViewModels
 {
     public class AnalyticsViewModel
     {
         public int ID { get; set; }
+        [Display(Name = "Username")]
         public string AuthorName { get; set; }
+        [Display(Name = "Number of Comments")]
         public int NumComments { get; set; }
     }
 }
